2c918fe3a559e2879feb58c8298eb6935399197e
Bruno Sampaio <Bruno@c-3po.home>
2013-07-13 11:56:13 +0100

    v1.0.7
    German translation added

---
+++ lang/de/block_gchat.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * Strings for component 'block_gchat', language 'de', branch 'MOODLE_20_STABLE'
+ *
+ * @package   block_gchat
+ * @copyright 2013 Bruno Sampaio
+ * Translated by Florian Dehmel
+ */
+
+$string['pluginname'] = 'Globaler Chat';
+
+//General
+$string['loading'] = 'Laden';
+$string['send-message'] = 'Senden';
+$string['no-users'] = 'Keine Nutzer online.';
+$string['no-support'] = 'Dein Brownser unterstützt WebSockets nicht, was für
+das korrekte Funktionieren dieses Plugins benötigt wird. Bitte auf die letzte
+Version upgraden oder noch einmal probieren.';
+$string['connection-lost'] = 'Die Verbindung zum Server fehlt!';
+
+//Settings
+$string['server_name'] = 'Server-Name';
+$string['server_name_desc'] = 'Der Name wird verwendet, um sich mit dem Server
+zu verbinden. Beispiele: localhost, www.moodle.com.';
+
+$string['server_port'] = 'Server-Port';
+$string['server_port_desc'] = 'Der Port wird verwendet, um sich mit dem Server
+zu verbinden.';
+
+$string['chat_container'] = 'Chat Container';
+$string['chat_container_desc'] = 'Der Chat Container ist ein DOM-Element auf
+einer Seite. Choose the element with the main scroll, usually it is the body,
+but other themes can use the element with id page.';
+
+$string['header_color'] = 'Farbe Chatfenster (Normal)';
+$string['header_color_desc'] = 'Hintergrundfarbe für das Chatfenster.';
+
+$string['header_new_messages_color'] = 'Farbe Chatfenster (Neue Nachrichten)';
+$string['header_new_messages_color_desc'] = 'Hintergrundfarbe des Chatfensters
+für ungelesene Nachrichten.';
+
+$string['header_text_color'] = 'Textfarbe Chatfenster';
+$string['header_text_color_desc'] = 'Textfarbe im Chatfenster.';
+
+$string['window_border_color'] = 'Konturfarbe Chatfenster';
+$string['window_border_color_desc'] = 'Konturfarbe im Chatfenster.';
